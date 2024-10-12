2097f1a1068586d27af60e068289fe907d2e306d
AhmedEssam0271 <ahmedessam0271@gmail.com>
2024-10-13 01:01:10 +0300

    adjustments

--- quiz.js
+++ quiz.js
@@ -1,28 +1,23 @@
-
-
 // document.querySelector('quiz');
 function checkAnswer() {
-    // Function body
+  // Function body
 
-    let userAnswer;
-    const correctAnswer="4";
-    if(document.getElementById("choice1").checked){
-    userAnswer =document.getElementById("choice1").value;
-    }
-    else if(document.getElementById("choice2").checked){
-    userAnswer =document.getElementById("choice2").value;
-    }
-    else if(document.getElementById("choice3").checked){
-    userAnswer =document.getElementById("choice3").value;
-    }
+  let userAnswer;
+  const correctAnswer = "4";
+  if (document.getElementById("choice1").checked) {
+    userAnswer = document.getElementById("choice1").value;
+  } else if (document.getElementById("choice2").checked) {
+    userAnswer = document.getElementById("choice2").value;
+  } else if (document.getElementById("choice3").checked) {
+    userAnswer = document.getElementById("choice3").value;
+  }
 
-    if (userAnswer === correctAnswer){
-        document.getElementById("feedback").innerHTML="Correct! Well done.";
-    }
-    else{
-        document.getElementById("feedback").innerHTML="That's incorrect. Try again!";
-    }
-
+  if (userAnswer === correctAnswer) {
+    document.getElementById("feedback").innerHTML = "Correct! Well done.";
+  } else {
+    document.getElementById("feedback").innerHTML =
+      "That's incorrect. Try again!";
+  }
 }
 
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
